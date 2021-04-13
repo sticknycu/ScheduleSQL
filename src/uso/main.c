0cabe07d9473c675ec1fb62754f821aac26c7925
nicugnm <nicolae.ghergu@s.unibuc.ro>
2021-04-13 19:15:30 +0300

    Initial project base. Add Makelist and basic main/.c .h Adding pragma for clang

---
+++ src/uso/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Because SIG_DFL or SIG_IGN could be NULL instead of nullptr, we need to disable the Clang warning here
+#ifdef __clang__
+    #pragma clang diagnostic push
+	#pragma clang diagnostic ignored "-Wunknown-warning-option"
+	#pragma clang diagnostic ignored "-Wunknown-pragmas"
+	#pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
+#endif  // __clang__
+
+
+int main(int argc, char** argv)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ src/uso/main.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifndef SCHEDULESQL_MAIN_H
+
+#define SCHEDULESQL_MAIN_H
+
+#endif //SCHEDULESQL_MAIN_H
