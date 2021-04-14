013cd97f25e02ca5be994f8ef70da94d139bbb6f
nicugnm <nicolae.ghergu@s.unibuc.ro>
2021-04-14 22:33:23 +0300

    Trying arguments and start some tests.

--- src/uso/main.c
+++ src/uso/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Because SIG_DFL or SIG_IGN could be NULL instead of nullptr, we need to disable the Clang warning here
 #ifdef __clang__
@@ -9,8 +11,39 @@
 #endif  // __clang__
 
 
-int main(int argc, char** argv)
-{
-    printf("Bonjour, Monde!\n");
+int main(int argc, char** argv) {
+    char src[50], dest[50];
+
+    // program.exe -c BazaDeDate.json // CREATE
+    // program.exe -u BazaDeDate.json -i angajati -t nume -m Albert // INSERT
+    // program.exe -d BazaDeDate.json // DELETE
+
+    puts("");
+    printf("Argumente: %s %s" , argv[1], argv[2]);
+    /*if (strcmp(argv[1], "-u") == 0) {
+        // baza de date = BazaDeDate.json
+        if (strcmp(argv[3], "-i")) {
+            // inseram
+
+        }
+    }*/
+    if (strcmp(argv[1], "-c") == 0) {
+        // scriem codul pentru create
+        // touch BazaDeDate.sql
+        strcpy(src,  argv[2]);
+        strcpy(dest, "touch ");
+        // Concatenare "touch " + "BazaDeDate.json"
+        // Pentru a face intr-un string "touch BazaDeDate.json"
+        // system("touch BazaDeDate.json");
+
+        strcat(dest, src);
+        system(dest);
+    }
+
+    if (strcmp(argv[1], "-d") == 0) {
+        puts("");
+        remove(argv[2]);
+    }
+    puts("");
     return 0;
 }
